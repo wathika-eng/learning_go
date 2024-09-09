5714a6251115733c79c7143825d7aeb49d67165b
wathika-eng <wathika02@gmail.com>
2024-09-09 23:04:21 +0300

    Refactor bill calculation logic and improve input validation

--- project_3/main.go
+++ project_3/main.go
@@ -6,13 +6,14 @@ func main() {
 	var amount, tip float64
 	fmt.Printf("Enter the amount in Ksh: ")
 	fmt.Scanf("%f", &amount)
-	if amount < 0 {
+	fmt.Printf("Enter tip for waiter: ")
+	fmt.Scanf("%f", &tip)
+	if amount < 0 || tip < 0 {
 		fmt.Println("Enter correct amount of money!")
 		return
 	}
-	fmt.Printf("Enter tip for waiter: ")
-	fmt.Scanf("%f", &tip)
 	fmt.Printf("Amount is: Ksh.%.2f\t Tip is: Ksh.%.2f\n", amount, tip)
-	bill := amount + tip
+	tipPercent := amount * (tip / 100)
+	bill := amount + tipPercent
 	fmt.Printf("Total bill: Ksh.%.2f\n", bill)
 }
