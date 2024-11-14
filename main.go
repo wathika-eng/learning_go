a041876bb27207909d8ce467860e75d11ef7d4c5
wathika-eng <wathika02@gmail.com>
2024-11-14 20:59:02 +0300

    Refactor main.go: Update product list handling and improve output formatting

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ func main() {
 	productNames[0] = "Cherry"
 	pl(productNames) // array is overwritten
 	newProductsList := append(productNames, "Kiwi")
-	fmt.Printf("Len: %v, Cap: %v", len(productNames), cap(newProductsList)) // 3, capacity becomes x2 of len
+	fmt.Printf("Len: %v, Cap: %v\n", len(newProductsList), cap(newProductsList)) // 4, capacity becomes x2 or x1.5 of len
+	pl(newProductsList)                                                          // kiwi comes last
+	pl(newProductsList[len(newProductsList)-1])                                  // we have 4 elements but we need last index and we start counting with 0
+	fmt.Printf("Type is %T", productNames)
 }
 
 //short fmt.Println(s any)
